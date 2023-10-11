ea35e92429a6765b9e905d16acfc1dda653225c0
ProgRaizell <francois.glav@hotmail.com>
2023-10-11 14:45:42 +0200

    remplissage de transisiton et place

--- Place.java
+++ Place.java
@@ -1,7 +1,32 @@
 
 public class Place {
 
+    private int token;
+    private String name;
 
 
+    public void remove(){
+
+
+    }
     
+    public void add(int a){
+
+        
+    }
+
+    public int getTokens(){
+		return token;
+        
+    }
+
+    public String getName(){
+		return name;
+        
+    }
+
+    public boolean isTokenSup(){
+		return false;
+
+    }
 }
--- Transition.java
+++ Transition.java
@@ -1,3 +1,12 @@
 public class Transition {
-    
+    private String name;
+
+    public void step(){
+
+    }
+
+    public String getName(){
+		return name;
+    }
+
 }
