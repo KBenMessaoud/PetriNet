f10d5f3c21dc8f4947a277f763a4f369c0b3baf5
KBenMessaoud <karim.bmessaoud@gmail.com>
2023-10-11 14:51:29 +0200

    Ajout des liens entre transitions et inEdge/out

--- Transition.java
+++ Transition.java
@@ -1,6 +1,12 @@
-public class Transition {
-    private String name;
+import java.util.ArrayList;
 
+public class Transition {
+    
+    private String name;
+    private ArrayList<InEdge> inEdgeList = new ArrayList<>();
+    private ArrayList<OutEdge> outEdgeList = new ArrayList<>(); // Added ArrayList of OutEdge
+    
+    
     public void step(){
 
     }
