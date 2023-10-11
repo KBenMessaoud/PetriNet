78632df705f4437de4c3470ca8fee23d0f1c713c
ProgRaizell <francois.glav@hotmail.com>
2023-10-11 14:37:38 +0200

    remplissage de PetriNetImplement

--- PetriNetImplements.java
+++ PetriNetImplements.java
@@ -1,3 +1,8 @@
-public class PetriNetImplements {
+import java.util.ArrayList;
+
+public class PetriNetImplements implements petrinet{
     
+    private ArrayList<Transition> transistions = new ArrayList<Transition>();
+    private ArrayList<Place> places = new ArrayList<Place>();
+
 }
