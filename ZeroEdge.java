682014390354b23518fc8b595e3ad97c37d16091
ProgRaizell <francois.glav@hotmail.com>
2023-10-11 15:43:22 +0200

    changement zeroEdge

--- ZeroEdge.java
+++ ZeroEdge.java
@@ -5,18 +5,24 @@ public class ZeroEdge extends OutEdge{
         //TODO Auto-generated constructor stub
     }
 
+    //trigger: active la transition lorsque la place source est vide.
 
     @Override
     public void trigger() {
-        if(this.getValue()> place.getTokens()){
-            System.out.println("Not enough tokens");
+        if(isTriggerable() == true ){
+            System.out.println("You have no token in this place");
         }
-        
     }
 
     @Override
     public boolean isTriggerable() {
-        return true;
+
+        if (super.place.getTokens() == 0){
+            return true;
+        }else{
+            return false;
+        }
+
     }
     
 }
