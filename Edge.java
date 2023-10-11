1267f69fde3663c8f382d9fa87d5b2d238c911c4
KBenMessaoud <karim.bmessaoud@gmail.com>
2023-10-11 14:38:04 +0200

    Edge début + inEdge début

--- Edge.java
+++ Edge.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Edge {
     
+    private int value;
+
+    public Edge(int value){
+
+        this.value = value;
+    }
+
+
+    public void trigger(){
+        System.out.println("triggered");
+    }
+    
+    
+    
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
 }
--- InEdge.java
+++ InEdge.java
@@ -1,3 +1,18 @@
-public class InEdge {
+public class InEdge extends Edge {
+
+    
+    
+    
+    
+    public InEdge(int value) {
+        super(value);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public void trigger() {
+        
+    }
     
 }
